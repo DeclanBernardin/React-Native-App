efe71b46d12a58c53433bd8f619960a014d565be
Declan Bernardin <dmjbernardin@yahoo.com>
2019-11-25 21:02:28 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, { useState }from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  const [outputText, setOutputText] = useState('React Native');
+  return (
+    <View style={styles.container}>
+      <Text>{outputText}</Text>
+      <Button title="Change Text" onPress={() => setOutputText('Text Changed!') }/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
