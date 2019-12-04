a22624515e6fc6e4450b8faa8ec221fc729f6f57
Declan Bernardin <dmjbernardin@yahoo.com>
2019-12-03 20:54:28 -0600

    added a cancel button

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,36 +1,53 @@
 import React, {useState} from 'react';
-import {View, TextInput, Button, StyleSheet } from 'react-native';
+import {View, TextInput, Button, StyleSheet, Modal} from 'react-native';
 
 const GoalInput = props => {
     const [enteredGoal, setEnteredGoal] = useState(''); 
 
     const goalInputHandler = (enteredText) => {
         setEnteredGoal(enteredText);
-    }
+    };
+
+    const addGoalHandler = () => {
+        props.onAddGoal(enteredGoal);
+        setEnteredGoal('');
+    };
+
     return (
+        <Modal visible={props.visible} animationType="slide">
         <View style={styles.inputContainer}>
             <TextInput
                 placeholder="Type Here"
                 style={styles.input}
                 onChangeText={goalInputHandler}
                 value={enteredGoal} />
-
-            <Button title="Add" onPress={props.onAddGoal.bind(this, enteredGoal)} />
+            <View style={styles.buttonContainer}>
+                <Button title="CANCEL" color="red" onPress={props.onCancel}/>
+                <Button title="Add" onPress={addGoalHandler} />
+            </View>
         </View>
+        </Modal>
     );
 };
 
 const styles = StyleSheet.create({
     inputContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        flex: 1,
+        justifyContent: 'center',
         alignItems: 'center'
     },
     input: {
         padding: 10,
         width: '80%',
         borderBottomColor: 'black',
-        borderWidth: 1
-    },});
+        borderWidth: 1,
+        marginBottom: 10
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '40%',
+    }
+});
 
 export default GoalInput;
